f620188a9573a17ddec6fbb4058a9243def4802b
曹书豪 <caoshuhao@touke.rcom>
2018-03-12 17:32:50 +0800

    monitor thread naming

--- condition-order-monitor/src/main/java/me/caosh/condition/infrastructure/eventbus/MonitorEventBus.java
+++ condition-order-monitor/src/main/java/me/caosh/condition/infrastructure/eventbus/MonitorEventBus.java
@@ -1,8 +1,11 @@
 package me.caosh.condition.infrastructure.eventbus;
 
 import com.google.common.eventbus.AsyncEventBus;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by caosh on 2017/8/16.
@@ -10,5 +13,7 @@ import java.util.concurrent.Executors;
  * @author caoshuhao@touker.com
  */
 public class MonitorEventBus {
-    public static final AsyncEventBus EVENT_SERIALIZED = new AsyncEventBus(Executors.newSingleThreadExecutor());
+    public static final AsyncEventBus EVENT_SERIALIZED = new AsyncEventBus(new ThreadPoolExecutor(1, 1,
+            0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(),
+            new CustomizableThreadFactory("MonitorThread-")));
 }
