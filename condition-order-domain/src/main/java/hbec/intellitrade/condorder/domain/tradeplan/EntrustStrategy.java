62fa3f868fc9b68e2f6caa54395f6daa24b1e191
曹书豪 <caoshuhao@touker.com>
2018-03-24 14:59:04 +0800

    optimize(module): 完善五档择机逻辑，五档价格为空或0时使用当前价

--- condition-order-domain/src/main/java/hbec/intellitrade/condorder/domain/tradeplan/EntrustStrategy.java
+++ condition-order-domain/src/main/java/hbec/intellitrade/condorder/domain/tradeplan/EntrustStrategy.java
@@ -1,6 +1,6 @@
 package hbec.intellitrade.condorder.domain.tradeplan;
 
-import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
 import hbec.intellitrade.common.market.RealTimeMarket;
 import me.caosh.autoasm.ConvertibleEnum;
 
@@ -29,77 +29,77 @@ public enum EntrustStrategy implements ConvertibleEnum<Integer> {
     CURRENT_PRICE(1) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getCurrentPrice(), realTimeMarket.getPreviousPrice());
+            return firstValidOrSecond(realTimeMarket.getCurrentPrice(), realTimeMarket.getPreviousPrice());
         }
     },
     SELL5(2) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(0),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(0),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     SELL4(3) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(1),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(1),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     SELL3(4) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(2),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(2),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     SELL2(5) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(3),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(3),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     SELL1(6) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(4),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(4),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     BUY1(7) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(5),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(5),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     BUY2(8) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(6),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(6),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     BUY3(9) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(7),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(7),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     BUY4(10) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(8),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(8),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     BUY5(11) {
         @Override
         public BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket) {
-            return MoreObjects.firstNonNull(realTimeMarket.getOfferedPrices().get(9),
-                                            CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+            return firstValidOrSecond(realTimeMarket.getOfferedPrices().get(9),
+                                      CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
         }
     },
     MARKET_PRICE(99) {
@@ -128,4 +128,30 @@ public enum EntrustStrategy implements ConvertibleEnum<Integer> {
      * @return 委托价格，可能为空
      */
     public abstract BigDecimal selectEntrustPrice(RealTimeMarket realTimeMarket);
+
+    /**
+     * 如果第一个价格有效，选择第一个，否则断言第二个有效并返回第二个，都无效的情况下抛出异常
+     *
+     * @param first  参数1
+     * @param second 参数2
+     * @return 有效参数
+     */
+    public static BigDecimal firstValidOrSecond(BigDecimal first, BigDecimal second) {
+        if (isValidPrice(first)) {
+            return first;
+        }
+
+        Preconditions.checkArgument(isValidPrice(second), "Second argument is not valid");
+        return second;
+    }
+
+    /**
+     * 价格是否有效
+     *
+     * @param price 五档价格，可能为空
+     * @return 有效
+     */
+    public static boolean isValidPrice(BigDecimal price) {
+        return price != null && price.compareTo(BigDecimal.ZERO) > 0;
+    }
 }
--- condition-order-domain/src/test/java/hbec/intellitrade/condorder/domain/tradeplan/EntrustStrategyTest.java
+++ condition-order-domain/src/test/java/hbec/intellitrade/condorder/domain/tradeplan/EntrustStrategyTest.java
@@ -2,8 +2,9 @@ package hbec.intellitrade.condorder.domain.tradeplan;
 
 import com.google.common.collect.Lists;
 import hbec.intellitrade.common.market.MarketID;
+import hbec.intellitrade.common.market.MarketType;
 import hbec.intellitrade.common.market.RealTimeMarket;
-import hbec.intellitrade.common.security.SecurityType;
+import hbec.intellitrade.mock.MockMarkets;
 import org.joda.time.LocalDateTime;
 import org.testng.annotations.Test;
 
@@ -18,12 +19,22 @@ import static org.testng.Assert.assertEquals;
 public class EntrustStrategyTest {
     @Test
     public void test() throws Exception {
-        MarketID pfyh = new MarketID(SecurityType.STOCK, "600000");
+        RealTimeMarket realTimeMarket = MockMarkets.withCurrentPrice(new BigDecimal("10.00"));
+        assertEquals(new BigDecimal("10.00"), EntrustStrategy.CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
+        assertEquals(new BigDecimal("10.04"), EntrustStrategy.SELL5.selectEntrustPrice(realTimeMarket));
+        assertEquals(new BigDecimal("10.00"), EntrustStrategy.SELL1.selectEntrustPrice(realTimeMarket));
+        assertEquals(new BigDecimal("9.99"), EntrustStrategy.BUY1.selectEntrustPrice(realTimeMarket));
+        assertEquals(new BigDecimal("9.95"), EntrustStrategy.BUY5.selectEntrustPrice(realTimeMarket));
+    }
+
+    @Test
+    public void testInvalid() throws Exception {
+        MarketID pfyh = new MarketID(MarketType.STOCK, "600000");
         RealTimeMarket realTimeMarket = new RealTimeMarket(pfyh,
                                                            new BigDecimal("10.00"),
                                                            new BigDecimal("10.00"),
                                                            Lists.newArrayList(
-                                                                   new BigDecimal("10.04"),
+                                                                   null,
                                                                    new BigDecimal("10.03"),
                                                                    new BigDecimal("10.02"),
                                                                    new BigDecimal("10.01"),
@@ -32,12 +43,11 @@ public class EntrustStrategyTest {
                                                                    new BigDecimal("9.98"),
                                                                    new BigDecimal("9.97"),
                                                                    new BigDecimal("9.96"),
-                                                                   new BigDecimal("9.95")),
+                                                                   new BigDecimal("0.00")),
+                                                           LocalDateTime.now(),
                                                            LocalDateTime.now());
         assertEquals(new BigDecimal("10.00"), EntrustStrategy.CURRENT_PRICE.selectEntrustPrice(realTimeMarket));
-        assertEquals(new BigDecimal("10.04"), EntrustStrategy.SELL5.selectEntrustPrice(realTimeMarket));
-        assertEquals(new BigDecimal("10.00"), EntrustStrategy.SELL1.selectEntrustPrice(realTimeMarket));
-        assertEquals(new BigDecimal("9.99"), EntrustStrategy.BUY1.selectEntrustPrice(realTimeMarket));
-        assertEquals(new BigDecimal("9.95"), EntrustStrategy.BUY5.selectEntrustPrice(realTimeMarket));
+        assertEquals(new BigDecimal("10.00"), EntrustStrategy.SELL5.selectEntrustPrice(realTimeMarket));
+        assertEquals(new BigDecimal("10.00"), EntrustStrategy.BUY5.selectEntrustPrice(realTimeMarket));
     }
 }
