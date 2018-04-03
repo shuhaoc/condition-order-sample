e6b4f0ba7b67f19ce1292f251ec9755efcacb41a
曹书豪 <caoshuhao@touker.com>
2018-04-03 18:24:10 +0800

    feat(location): InputStreamObject extends Serializable

--- condition-order-domain/src/main/java/hbec/intellitrade/replay/InputStreamObject.java
+++ condition-order-domain/src/main/java/hbec/intellitrade/replay/InputStreamObject.java
@@ -1,12 +1,14 @@
 package hbec.intellitrade.replay;
 
+import java.io.Serializable;
+
 /**
  * 输入流对象
  *
  * @author caosh/caoshuhao@touker.com
  * @date 2018/3/28
  */
-public interface InputStreamObject {
+public interface InputStreamObject extends Serializable {
     /**
      * 获取输入时间戳，决定其在输入流中的顺序
      *
